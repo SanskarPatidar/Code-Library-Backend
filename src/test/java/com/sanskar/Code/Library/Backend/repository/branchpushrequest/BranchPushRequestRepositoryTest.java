804d99cc9c511d8c399a2a72f1a4f40c54a93b38
Sanskar Patidar <sanskarpatidar00@gmail.com>
2025-08-30 20:23:47 +0530

    last commit

--- src/test/java/com/sanskar/Code/Library/Backend/repository/branchpushrequest/BranchPushRequestRepositoryTest.java
+++ src/test/java/com/sanskar/Code/Library/Backend/repository/branchpushrequest/BranchPushRequestRepositoryTest.java
@@ -1,70 +1,56 @@
 package com.sanskar.Code.Library.Backend.repository.branchpushrequest;
 
-import com.sanskar.Code.Library.Backend.model.BranchPushRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@DataMongoTest
-@ActiveProfiles("dev")
-class BranchPushRequestRepositoryTest {
-
-    @Autowired
-    BranchPushRequestRepository repository;
-
-    @BeforeEach
-    void setUp() {
-        repository.deleteAll();
-
-        BranchPushRequest req1 = BranchPushRequest.builder()
-                .id("1")
-                .requestedBy("user1")
-                .requestedAt(LocalDateTime.now().minusMinutes(10))
-                .snippetId("s1")
-                .build();
-
-        BranchPushRequest req2 = BranchPushRequest.builder()
-                .id("2")
-                .requestedBy("user1")
-                .requestedAt(LocalDateTime.now())
-                .snippetId("s2")
-                .build();
-
-        BranchPushRequest req3 = BranchPushRequest.builder()
-                .id("3")
-                .requestedBy("user1")
-                .requestedAt(LocalDateTime.now().plusMinutes(10))
-                .snippetId("s1")
-                .rejected(true)
-                .build();
-
-        repository.saveAll(List.of(req1, req2, req3));
-    }
-
-    @Test
-    void findByRequesterUsernameOrderByRequestedAtDescTest(){
-        var page = repository.findByRequesterUsernameOrderByRequestedAtDesc("user1", PageRequest.of(0, 10));
-        assertEquals(3, page.getTotalElements());
-        List<LocalDateTime> timeList = page.getContent()
-                .stream()
-                .map(BranchPushRequest::getRequestedAt)
-                .toList();
-        assertThat(timeList).isSortedAccordingTo(Comparator.reverseOrder());
-    }
-
-    @Test
-    void findBySnippetIdValidTest(){
-        var page = repository.findAllBySnippetIdAndValidAsPage("s1", PageRequest.of(0, 10));
-        assertEquals(1, page.getTotalElements());
-    }
-}
+//@DataMongoTest
+//@ActiveProfiles("dev")
+//@Disabled
+//class BranchPushRequestRepositoryTest {
+//
+//    @Autowired
+//    BranchPushRequestRepository repository;
+//
+//    @BeforeEach
+//    void setUp() {
+//        repository.deleteAll();
+//
+//        BranchPushRequest req1 = BranchPushRequest.builder()
+//                .id("1")
+//                .requestedBy("user1")
+//                .requestedAt(LocalDateTime.now().minusMinutes(10))
+//                .snippetId("s1")
+//                .build();
+//
+//        BranchPushRequest req2 = BranchPushRequest.builder()
+//                .id("2")
+//                .requestedBy("user1")
+//                .requestedAt(LocalDateTime.now())
+//                .snippetId("s2")
+//                .build();
+//
+//        BranchPushRequest req3 = BranchPushRequest.builder()
+//                .id("3")
+//                .requestedBy("user1")
+//                .requestedAt(LocalDateTime.now().plusMinutes(10))
+//                .snippetId("s1")
+//                .rejected(true)
+//                .build();
+//
+//        repository.saveAll(List.of(req1, req2, req3));
+//    }
+//
+//    @Test
+//    void findByRequesterUsernameOrderByRequestedAtDescTest(){
+//        var page = repository.findByRequesterUsernameOrderByRequestedAtDesc("user1", PageRequest.of(0, 10));
+//        assertEquals(3, page.getTotalElements());
+//        List<LocalDateTime> timeList = page.getContent()
+//                .stream()
+//                .map(BranchPushRequest::getRequestedAt)
+//                .toList();
+//        assertThat(timeList).isSortedAccordingTo(Comparator.reverseOrder());
+//    }
+//
+//    @Test
+//    void findBySnippetIdValidTest(){
+//        var page = repository.findAllBySnippetIdAndValidAsPage("s1", PageRequest.of(0, 10));
+//        assertEquals(1, page.getTotalElements());
+//    }
+//}
